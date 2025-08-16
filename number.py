a16169dd28115ed88f0f1da49b7a620238436a8b
MD.Shimul <md.shimuldiu@gmail.com>
2025-08-16 09:19:29 +0600

    added string property

---
+++ number.py
@@ -0,0 +1,7 @@
+import random
+# print(random.randint(4, 9))
+print(random.randrange(4, 8, 2))
+sequence=[8,9,'sdjd','dfj']
+print(random.choice(sequence))
+random.seed(8)
+print(random.randint(7, 90))
---
+++ string.py
@@ -0,0 +1,50 @@
+multiLineStr="""hi i am shimul
+i have one brother
+i live in dhaka
+and stardy university
+"""
+print(multiLineStr)
+text="HELLO word"
+print('HELLO' in text)
+print('HI'not in text)
+Name="Md. Shimul"
+print(Name[-2:-6:-1])
+name='lmy name is shimul '
+print(name)
+name.strip('l')
+print(name)
+name='Md. Shimuld'
+name.replace('d', 'k')
+print(name)
+# formating string
+age=9
+print(f"i am {age} years old")
+a=9
+b=0
+print(f"first num {a} + second num {b} =sum is {a+b}")
+pi=4.5445
+print(f"pi is {pi:.2f}")
+num=9.0000000
+print(f"{num:.2f}")
+print("Don\'t")
+print("My name is \"Shimul\"")
+# string mathods
+name="Md. Shimul"
+capital=name.capitalize()
+print(capital)
+small=name.casefold()
+text="shimul"
+print(text.center(80))
+text='appsanad'
+print(text.count('a',0,4))
+text='shimul akram'
+text='a\tv\tf'
+print(text.endswith('d'))
+print(text.expandtabs())
+print(text.expandtabs(4))
+s='9'
+print(f"{int(s):b}")
+print(f"{int(s):o}")
+name='Md.shimul'
+print(name.upper())
+print(pow(4,0))
