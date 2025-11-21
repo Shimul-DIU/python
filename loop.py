4b0edef18240cf3d9d15a506c1caa884907864ff
MD.Shimul <miashimul6363@gmail.com>
2025-11-21 12:22:40 +0600

    try to fix bug

---
+++ loop.py
@@ -0,0 +1,26 @@
+# --------------for loop------------------
+for i in range(33):
+    print(i)
+for i in range(2,33,4):
+    if i%2==0:
+        print("even:",i)
+    else:
+        print("odd:",i)
+nums=[x*2 if x%2==0 else x for x in range(10)]
+print(nums)
+# -------------------while loop -------------------------
+i=0
+while i<10:
+    if i==3:
+        i+=1
+        continue
+    print(i*i)
+    i+=1
+    if i==5:
+      break
+j=1
+while j<10:
+    print(j)
+    j+=1
+else:
+    print("last number is 10")
