76706c3eaa5907f99b44e4aa2b97972956aa29bd
MD.Shimul <miashimul6363@gmail.com>
2025-11-28 09:24:59 +0600

    add filter,list comprehension,map

---
+++ filter.py
@@ -0,0 +1,3 @@
+nums=[1,2,3,4,5]
+result=list(filter(lambda x: x%2==1,nums))
+print(result)
---
+++ listComprehention.py
@@ -0,0 +1,5 @@
+result=[x for x in range(1,9) if x%2==0]
+print(result)
+arr=[1,2,3,4,5,6,7,8,9,10]
+evenAndGeter2=[x+1 for x in arr if x>2 and x%2==0]
+print(evenAndGeter2)
